1851fa697b851a74e01b1d18687dbdd97bf1fa2d
vinhphan812 <vinhphan812@gmail.com>
2022-05-19 19:57:10 +0700

    http server

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const http = require("http"),
 const testRoute = require("./routers/test.route");
 const { verify, chat } = require("./controllers/webhook.controller");
 
-const key = fs.readFileSync("./key.pem");
-const cert = fs.readFileSync("./cert.pem");
+// const key = fs.readFileSync("./key.pem");
+// const cert = fs.readFileSync("./cert.pem");
 
-const credentials = { key, cert };
+// const credentials = { key, cert };
 
 const app = express();
 
@@ -29,24 +29,24 @@ app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./index.html")));
 app.get("/webhook", verify);
 app.post("/webhook", chat);
 
-app.set("port", process.env.PORT || 80);
+app.set("port", process.env.PORT || 3000);
 app.set("ip", process.env.IP || "0.0.0.0");
 
 var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
+// var httpsServer = https.createServer(credentials, app);
 
-// httpServer.listen(app.get("port"), app.get("ip"), function () {
-// 	console.log(
-// 		"Chat bot server listening at %s:%d ",
-// 		app.get("ip"),
-// 		app.get("port")
-// 	);
-// });
-
-httpsServer.listen(app.get("port"), app.get("ip"), function () {
+httpServer.listen(app.get("port"), app.get("ip"), function () {
 	console.log(
 		"Chat bot server listening at %s:%d ",
 		app.get("ip"),
 		app.get("port")
 	);
 });
+
+// httpsServer.listen(app.get("port"), app.get("ip"), function () {
+// 	console.log(
+// 		"Chat bot server listening at %s:%d ",
+// 		app.get("ip"),
+// 		app.get("port")
+// 	);
+// });
